cf9c0cdb6e473a9096e90744e93785453b01e8e6
Lianping Chen <lchen@Lianpings-MacBook-M1.local>
2022-01-05 19:47:34 +0000

    added subnet

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ terraform {
 
 
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
 
@@ -33,12 +33,22 @@ resource "aws_instance" "web" {
   ami                    = "ami-830c94e3"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
+  subnet_id              = aws_subnet.main.id
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = "vpc-685a480a"
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
 }
 
 resource "aws_security_group" "web-sg" {
